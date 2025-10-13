afe3a0dde93d87ba8fdd9f94343cc89bcbb071bc
Manas Agrawal <agrawal.manas31@gmail.com>
2025-10-12 23:37:58 -0400

    Initialize Spring Boot project `stocksprout` with Maven wrapper, essential properties, dependencies, folder structure, and IntelliJ project files.

---
+++ backend/stocksprout/src/main/java/com/vampz/stocksprout/StocksproutApplication.java
@@ -0,0 +1,13 @@
+package com.vampz.stocksprout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksproutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksproutApplication.class, args);
+	}
+
+}
